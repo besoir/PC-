cf8c0079ef02b092a197d430d3db186faa05cf00
Tommy <thanley@oswego.edu>
2018-03-12 15:26:17 -0400

    csv file parser

--- Golf/src/main/java/GolfDatabase/Golf/Team.java
+++ Golf/src/main/java/GolfDatabase/Golf/Team.java
@@ -1,6 +1,9 @@
 package GolfDatabase.Golf;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Team {
 	private String teamName;
@@ -9,5 +12,23 @@ public class Team {
 	public Team(String tName) {
 		teamName = tName;
 		this.golferList = new ArrayList<Golfer>();
+	}
+	
+	public void readFile(String fileName) throws FileNotFoundException {
+		File toRead = new File(fileName);
+		Scanner fc = new Scanner(toRead);
+		while(!(fc.hasNextLine())) {
+			String garbage = fc.nextLine();
+			String garbageSplitter[] = garbage.split(" ");
+			Golfer temp = new Golfer(garbageSplitter[0], garbageSplitter[1]);
+			for(Golfer g:golferList) {
+				if(g.equals(temp))
+					g.addScore(garbageSplitter[2]);
+				else {
+					temp.addScore(garbageSplitter[2]);
+					golferList.add(temp);
+				}
+			}
+		}
 	}
 }
