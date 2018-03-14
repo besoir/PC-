07dbe30d9db45ab31e7bf3c2154fec7c2f9faf17
Tommy <thanley@oswego.edu>
2018-03-14 19:36:22 -0400

    Bien?

--- Golf/src/main/java/GolfDatabase/Golf/Main.java
+++ Golf/src/main/java/GolfDatabase/Golf/Main.java
@@ -6,12 +6,10 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		Team t = new Team("Thomas");
-		//t.readFile("scores.txt");
-		//t.testPdfParser();
-		//t.printAll();
-	Section section5 = new Section();
-	section5.createTeamsFromPdf();
-	section5.printMap();
+		t.readFile("scores.txt");
+		t.testPdfParser();
+		t.printAll();
+	
 	}
 
 }
