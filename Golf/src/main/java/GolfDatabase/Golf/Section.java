bbf6b50b383d25498f379ccb5a9e038d47bd778b
Tommy <thanley@oswego.edu>
2018-04-01 13:20:52 -0400

    cleaned up some prints and made the match class

--- Golf/src/main/java/GolfDatabase/Golf/Section.java
+++ Golf/src/main/java/GolfDatabase/Golf/Section.java
@@ -41,7 +41,7 @@ public class Section implements java.io.Serializable  {
         			switch(tmp) {
         			case 0: // scores
         				String[] score = line.split(" ");	
-        				System.out.println(score[score.length-1]+" players score");
+        				//System.out.println(score[score.length-1]+" players score");
         				if(score[score.length-1].equals("Scores")) 
         					break;
         				placeHolder.addScore(score[score.length-1]);
@@ -54,11 +54,11 @@ public class Section implements java.io.Serializable  {
         					String[] names = playerName.split("\\s+");
         					placeHolder.setFirstName(names[0]);
         					placeHolder.setLastName(names[1]);
-        					System.out.println(names[0]+":"+ names[1]);
+        					//System.out.println(names[0]+":"+ names[1]);
             			}
         				while(school.find()) {
         					String teamName = school.group(1).trim();
-        					System.out.println("aa: "+teamName);
+        					//System.out.println("aa: "+teamName);
         					placeHolder.setTeam(teamName);
         					golfers.add(placeHolder);
         					if(!teams.containsKey(teamName)) {
@@ -79,7 +79,7 @@ public class Section implements java.io.Serializable  {
 	
 	private void createPlayersFromPdf(ArrayList<Golfer> playersFromPdf) {
 		//will add the players to the team
-		System.out.println(playersFromPdf.size()+ " size of unsorted array");
+		//System.out.println(playersFromPdf.size()+ " size of unsorted array");
 		for(Golfer gf : playersFromPdf) {
 			if(teams.containsKey(gf.getTeam())) {
 				teams.get(gf.getTeam()).addPlayer(gf);
@@ -99,7 +99,7 @@ public class Section implements java.io.Serializable  {
 	public void addPlayer(Golfer gf , String teamName) {
 		if(teams.containsKey(teamName)) {
 			teams.get(teamName).addPlayer(gf);
-			System.out.println("A player was added");
+			//System.out.println("A player was added");
 		}
 	}
 	
